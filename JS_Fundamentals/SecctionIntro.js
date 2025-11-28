3e74b237673f7424f4bdcc5cafb05cedd8fef4d0
Hibrul Anam Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2025-11-29 02:48:17 +0600

    Getting started with JavaScript

---
+++ JS_Fundamentals/SecctionIntro.js
@@ -0,0 +1,10 @@
+// alert("Hello World!");
+console.log("Hello World!");
+
+alert("Hello World!");
+undefined
+let js = "amazing";
+undefined
+if(js == "amazing") alert("JavaScript is FUN!");
+undefined
+if(js === 'amazing') alert("Great!");
