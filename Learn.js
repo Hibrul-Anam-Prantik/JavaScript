297a3602da82c01ee3636f89f069313be65fc2be
Hibrul Anam Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2025-11-29 18:11:26 +0600

    Operator, Operator Precedence

--- Learn.js
+++ Learn.js
@@ -59,3 +59,52 @@ console.log(job);
 newVariable = 27;
 console.log(newVariable);
 
+// <==============> operators <==============>
+const now = 2025;
+const agePrantik = now - 2004;
+const ageSarah = now - 2010;
+console.log(agePrantik, ageSarah);
+
+console.log(agePrantik * 2, ageSarah / 2, 2 ** 3);  
+
+const firstName1 = 'Hibrul';
+const lastName1 = 'Prantik';
+console.log(firstName1 + ' ' + lastName1);  
+
+let x1 = 10 + 5;
+console.log(x1);
+x1 += 10; // x1 = x1 + 10
+console.log(x1);
+x1 *= 4; // x1 = x1 * 4 
+console.log(x1);
+x1++;
+console.log(x1);
+x1--;
+console.log(x1);
+
+console.log(agePrantik > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;  // boolean
+
+console.log(now - 2004 > now - 2010);
+
+
+// <==============> operator precedence <==============>
+const now1 = 2025;
+const agePrantik1 = now1 - 2004;
+const ageSarah1 = now1 - 2010;
+
+console.log(now1 - 2004 > now1 - 2010);
+
+// console.log(25 - 10 - 5);
+
+let x2, y2;
+x2 = y2 = 25 - 10 - 5; // y2 = 10 --> x2 = y2 --> x2 = 10
+console.log(x2, y2);
+
+const averageAge = (agePrantik1 + ageSarah1) / 2;
+console.log(agePrantik1, ageSarah1, averageAge);
+
+
+
