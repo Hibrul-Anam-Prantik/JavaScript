211b159a5fa6489b286bef1e05f4f380c70fae34
Hibrul Anam Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2025-12-02 10:59:50 +0600

    Refactor Functions.js to improve structure and readability --> Function Expression along with function declaration

--- Functions.js
+++ Functions.js
@@ -1,5 +1,25 @@
 'use strict';
+
+// function declaration
 function logger() {
     console.log('Logger Activated!');
 }
 logger();
+
+function juiceMaker(fruit, numFruits) {
+    console.log(`Making ${fruit} juice with ${numFruits} ${fruit}s.`);
+    const juice = fruit + ' juice';
+    return juice;
+}
+
+const juice1 = juiceMaker('Apple', 5);
+console.log(juice1, 'Served!');
+
+console.log(juiceMaker('Orange', 3), 'Served!');
+
+// function expression
+const calcAge = function (birthYear) {
+    return 2024 - birthYear;
+};
+const age1 = calcAge(2004);
+console.log('Age is:', age1);
