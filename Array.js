56469ce8aea2e2a68cb5e97e15ff93f980ae078c
Hibrul Anam Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2025-12-03 02:36:33 +0600

    Fix typo in friends array initialization and enhance array operations with push, pop, unshift, and shift methods

--- Array.js
+++ Array.js
@@ -10,10 +10,52 @@ fruits[1] = 'strawberry';
 console.log(fruits); // ['apple', 'strawberry', 'mango', 'banana']
 
 // another way to create an array
-const friedns = new Array('Sarah', 'Ayesha', 'David', 'Punk');
-console.log(friedns); // ['Sarah', 'Ayesha', 'David', 'Punk']
+const friends = new Array('Sarah', 'Ayesha', 'David', 'Punk', 'Void');
+console.log(friends); // ['Sarah', 'Ayesha', 'David', 'Punk', 'Void']
 
 const name1 = 'Prantik';
 const age = 20;
 const prantik = [name1, age, 3, 'programmer', fruits];
 console.log(prantik); 
+
+// exercise
+const calcAge = function(birthYear) {
+    return 2025 - birthYear;
+};
+const years = [2000, 2002, 2004, 2010, 2015];
+const ages = new Array();
+for(let i = 0; i < years.length; ++i) {
+    ages[i] = calcAge(years[i]);
+}
+let i = 0;
+while(i < ages.length) {
+    console.log(`${i+1}: ${friends[i]} ---> ${ages[i]} years old.`);
+    i++;
+}
+
+// array methods & operations -- using push, pop, unshift, shift, indexOf, includes -- on friends array
+// add elements at the end
+const newLength = friends.push('Robin'); // add element at the end & returns new length
+console.log(friends);
+console.log(newLength);
+// add element at the beginning
+friends.unshift('Luffy'); // add element at the beginning & returns new length
+console.log(friends);
+
+// remove element from the end
+const popped = friends.pop(); // removes last element & returns removed element
+console.log(`Removed element: ${popped}
+New Array: ${friends}`);
+
+// remove element from the beginning
+const shifted = friends.shift();
+console.log(`Removed element: ${shifted}
+New Array: ${friends}`);
+
+// indexOf
+console.log(friends.indexOf('David'));
+console.log(friends.indexOf('Zoro')); // -1 (not found)
+
+console.log(friends.includes('Ayesha')); // true
+console.log(friends.includes('Zoro')); // false
+
