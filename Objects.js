5eaa388e85c7dca66733ccd457b91ee0948174c7
Hibrul Anam Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2025-12-03 03:05:28 +0600

    Add object literal example with dynamic property access and validation

---
+++ Objects.js
@@ -0,0 +1,45 @@
+// object literal syntax
+// key-value pairs
+const person = {
+    // properties
+    firstName : 'first-name',
+    lastName : 'last-name',
+    age : 18,
+    job : 'programmer',
+    friends : ['friend1', 'friend2', 'friend3']
+};
+console.log(person);
+
+// accessing object properties -- dot vs bracket notation
+console.log(person.firstName); // dot notation
+console.log(person['lastName']); // bracket notation
+
+const nameKey = 'Name';
+console.log(person['first' + nameKey]); // firstName
+console.log(person['last' + nameKey]); // lastName
+
+person.lastName = 'Prantik';
+console.log(person.lastName);
+person.age = 20;
+console.log(person.age);
+
+// adding new properties
+person.location = 'Bangladesh';
+person['gmail'] = 'gsuit@g.bracu.ac.bd';
+console.log(person);
+
+// want to know if a property exists
+console.log(person.hasOwnProperty('job')); // true
+console.log(person.hasOwnProperty('salary')); // false
+
+// Want to know about an object's particular property value
+const wannaKnow = prompt("What do you want to know the person? [firstName, lastName, age, job, friedns, location, gmail]");
+
+console.log(person.wannaKnow); // undefined -- as there's no property named 'wannaKnow'
+console.log(person[wannaKnow]); // works fine -- dynamic property access
+
+if(person[wannaKnow]) {
+    console.log(person[wannaKnow]);
+} else {
+    console.log('Wrong request! Choose between firstName, lastName, age, job, friends, location, gmail');
+}
