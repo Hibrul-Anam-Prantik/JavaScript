6b08e85f23b87b56e1331d4d9e7cf1a02e1ef88c
Hibrul Anam Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2025-12-02 23:38:48 +0600

    array init - create, modify, get

---
+++ Array.js
@@ -0,0 +1,11 @@
+const fruits = ['apple', 'orange', 'mango', 'banana'];
+
+// accessing array elements
+console.log(fruits[0]); // apple
+console.log(fruits[2]); // mango
+console.log(fruits[fruits.length - 1]); // banana
+
+// modifyting array elements
+fruits[1] = 'strawberry';
+console.log(fruits); // ['apple', 'strawberry', 'mango', 'banana']
+
