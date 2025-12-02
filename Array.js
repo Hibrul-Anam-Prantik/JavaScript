bacbe70a28165fe5ca47f8c771d95034ffc18803
Hibrul Anam Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2025-12-02 23:44:38 +0600

    arrays --> can even store an array in an array

--- Array.js
+++ Array.js
@@ -9,3 +9,11 @@ console.log(fruits[fruits.length - 1]); // banana
 fruits[1] = 'strawberry';
 console.log(fruits); // ['apple', 'strawberry', 'mango', 'banana']
 
+// another way to create an array
+const friedns = new Array('Sarah', 'Ayesha', 'David', 'Punk');
+console.log(friedns); // ['Sarah', 'Ayesha', 'David', 'Punk']
+
+const name1 = 'Prantik';
+const age = 20;
+const prantik = [name1, age, 3, 'programmer', fruits];
+console.log(prantik); 
