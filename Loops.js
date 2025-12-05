c7981b08bfa01b47608df6575d41d4fe57a4a1fc
Hibrul Anam Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2025-12-05 23:20:42 +0600

    backward looping, nesteed loops, while loops

--- Loops.js
+++ Loops.js
@@ -1,6 +1,6 @@
 for(let i = 0; i < 5; ++i) {
     console.log('index number: ', i);
-
+}
 // continue and break statements
 const tempArr = ['Hello', 'World', 2025 - 2000, true, 'JavaScript'];
 console.log(tempArr);
@@ -28,4 +28,19 @@ for(let exercise = 1; exercise < 4; exercise++) {
     for(let rep = 1; rep <= 4; ++rep) {
         console.log(`  Weight Lifting Repetition ${rep} 🏋️‍♂️`);
     }
+}
+
+// usign for loop
+console.log('===== Using for loop =====');
+const friends = ['David', 'Ayesha', 'John', 'Michael'];
+for(let i = 0; i < friends.length; ++i) {
+    console.log(`${i+1}: ${friends[i]}`);
+}
+
+//using while loop
+console.log('===== Using while loop =====');
+let i = 0; 
+while(i < friends.length) {
+    console.log(`${i+1}: ${friends[i]}`);
+    i++;
 }
