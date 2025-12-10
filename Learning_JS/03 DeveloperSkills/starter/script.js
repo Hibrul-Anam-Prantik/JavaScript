468d6a820b98c276a91459ca39367a06ec9f027e
Hibrul Anam Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2025-12-10 11:53:36 +0600

    init

--- Learning_JS/03 DeveloperSkills/starter/script.js
+++ Learning_JS/03 DeveloperSkills/starter/script.js
@@ -6,6 +6,5 @@ if (x === 23) console.log(23);
 
 const birthYear = 2004;
 const calcAge = birthYear => 2025 - birthYear;
-
-console.log();
-console.log();
+console.log(calcAge(2004));
+console.log("Damn, I'm good at this JS stuff!");
