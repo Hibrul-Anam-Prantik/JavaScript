18f89cae56cbb2df750ac4a564fb512b4b2fb11d
Hibrul Anam Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2025-12-12 08:09:51 +0600

    Dom --> document.querySelector('.class').textContent, .value -- manipulation

--- Dom.js
+++ Dom.js
@@ -1,1 +1,9 @@
+"use strict";
 console.log("Dom.js file has been loaded successfully.");
+
+console.log(document.querySelector(".msg").textContent); // Getting text content - "Start guessing..."
+document.querySelector(".msg").textContent = "🎉 Correct Number!"; // Setting text content
+document.querySelector(".score").textContent = 13; // Setting score to 13
+document.querySelector(".number").textContent = 9;
+document.querySelector(".guess-number").value = 23; // Setting input value to 23
+console.log(document.querySelector(".guess-number").value); // Getting input value
